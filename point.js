6fafc905907822926d692e89e5523b08f620e040
mduccc <duc2521997@gmail.com>
2018-08-09 23:54:27 +0700

    commit

--- point.js
+++ point.js
@@ -12,42 +12,42 @@ const firebaseapp = firebase.initializeApp({
   	databaseURL: 'https://api-idol-272da.firebaseio.com'
 })
 
-app.use('/', (req, res, next)=>{
+app.use('/', (req, res, next) =>{
 	console.log('wellcome')
 	next()
 })
 
-app.use('/v1', (req, res, next)=>{
+app.use('/v1', (req, res, next) =>{
 	console.log('v1')
 	next()
 })
 
-app.use('/v1/idol', (req, res, next)=>{
+app.use('/v1/idol', (req, res, next) =>{
 	console.log('idol')
 	next()
 })
 
-app.use('/v1/profile', (req, res, next)=>{
+app.use('/v1/profile', (req, res, next) =>{
 	console.log('profile')
 	next()
 })
 
-app.use('/v1/song', (req, res, next)=>{
+app.use('/v1/song', (req, res, next) =>{
 	console.log('song')
 	next()
 })
 
-app.use('/v1/album', (req, res, next)=>{
+app.use('/v1/album', (req, res, next) =>{
 	console.log('album')
 	next()
 })
 
-app.use('/v1/mv', (req, res, next)=>{
+app.use('/v1/mv', (req, res, next) =>{
 	console.log('mv')
 	next()
 })
 
-app.use('/admin', (req, res, next)=>{
+app.use('/admin', (req, res, next) =>{
 	console.log('admin')
 	next()
 })
