b619b4c45efd679d644a3ccd0ed366947e68255e
mduccc <duc2521997@gmail.com>
2018-08-13 00:46:17 +0700

    commit

--- app_modules/v1/mv.js
+++ app_modules/v1/mv.js
@@ -17,16 +17,27 @@ class Mv{
 	}
 
 	mv(snap){
+		var albumObj = snap.album
+		var mvObj = snap.mv
 		var mv = []
-		for(var item in snap){
-			for(var itemChild in snap[item].song){
+		for(var item in albumObj){
+			console.log(albumObj[item].song)
+			for(var itemChild in albumObj[item].song){
 				mv.push({
 					key: itemChild,
-					name: snap[item].song[itemChild].name,
-					youtube: snap[item].song[itemChild].youtube,
-					lyrics: snap[item].song[itemChild].lyrics
+					name: albumObj[item].song[itemChild].name,
+					youtube: albumObj[item].song[itemChild].youtube,
+					lyrics: albumObj[item].song[itemChild].lyrics
 				})
 			}
+		}
+		for(var item in mvObj){
+			mv.push({
+				key: null,
+				name: null,
+				youtube: mvObj[item].youtube,
+				lyrics: null
+			})
 		}
 		return mv
 	}
@@ -41,7 +52,7 @@ class Mv{
 				if(child.val().name == 'blackpink'){
 					res.json({
 						idol: name,
-						mv: this.mv(child.val().album)
+						mv: this.mv(child.val())
 					})
 					res.end()
 					console.log({
